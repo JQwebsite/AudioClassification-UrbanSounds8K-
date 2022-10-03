f326d9b8cd6db9d10382656642fd406b81c83c86
J Q <jianquan08@gmail.com>
2022-10-03 11:30:22 +0800

    self. to augmentation code

--- Augmentation.py
+++ Augmentation.py
@@ -3,15 +3,17 @@ import torchaudio
 import random
 
 
-class Augment():
+class Augmentor():
 
-    def __init__(self):
-        pass
+    def __init__(self, audio_duration, audio_channels, audio_sampling):
+        self.audio_duration = audio_duration
+        self.audio_channels = audio_channels
+        self.audio_sampling = audio_sampling
 
-    def pad_trunc(self, aud, max_ms):
+    def pad_trunc(self, aud):
         sig, sr = aud
         num_rows, sig_len = sig.shape
-        max_len = int(sr / 1000 * max_ms)
+        max_len = int(sr / 1000 * self.audio_duration)
 
         if (sig_len > max_len):
             # Truncate the signal to the given length
@@ -29,12 +31,12 @@ class Augment():
             sig = torch.cat((pad_begin, sig, pad_end), 1)
         return (sig, sr)
 
-    def rechannel(self, aud, new_channel):
+    def rechannel(self, aud):
         sig, sr = aud
-        if (sig.shape[0] == new_channel):
+        if (sig.shape[0] == self.audio_channels):
             # Nothing to do
             return aud
-        elif (new_channel == 1):
+        elif (self.audio_channels == 1):
             # Convert from stereo to mono by selecting only the first channel
             resig = sig[:1, :]
         else:
@@ -42,16 +44,18 @@ class Augment():
             resig = torch.cat([sig, sig])
         return ((resig, sr))
 
-    def resample(self, aud, newsr):
+    def resample(self, aud):
         sig, sr = aud
-        if (sr == newsr):
+        if (sr == self.audio_sampling):
             # Nothing to do
             return aud
         num_channels = sig.shape[0]
         # Resample first channel
-        resig = torchaudio.transforms.Resample(sr, newsr)(sig[:1, :])
+        resig = torchaudio.transforms.Resample(sr,
+                                               self.audio_sampling)(sig[:1, :])
         if (num_channels > 1):
             # Resample the second channel and merge both channels
-            retwo = torchaudio.transforms.Resample(sr, newsr)(sig[1:, :])
+            retwo = torchaudio.transforms.Resample(sr, self.audio_sampling)(
+                sig[1:, :])
             resig = torch.cat([resig, retwo])
-        return ((resig, newsr))
+        return ((resig, self.audio_sampling))
